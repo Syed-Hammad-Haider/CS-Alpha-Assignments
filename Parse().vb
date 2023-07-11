966725860a793c458f0677846fd6112e004b208e
Syed-Hammad-Haider <116620289+Syed-Hammad-Haider@users.noreply.github.com>
2023-07-11 09:54:21 +0500

    Add files via upload

---
+++ Parse().vb
@@ -0,0 +1,42 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim a As String
+        Dim b As Integer
+        a = "978"
+        b = CInt(a)
+        Console.WriteLine(b)
+        Console.ReadKey()
+        'Dim comma, character As Char
+        'Dim index, count, number, total As Integer
+        'Dim strnum, Instring As String
+        'Dim avg As Decimal
+
+        'Console.WriteLine("Enter your series of whole numbers(separated by commas)")
+        'Instring = Console.ReadLine
+
+        'comma = ","
+        'For index = 1 To Len(Instring)
+        'character = Mid(Instring, index, 1)
+        'If character <> comma Then
+        'strnum = strnum & character
+        'Else
+        'number = CInt(strnum)
+        'total = total + number
+        'count = count + 1
+        'End If
+
+
+        'Next
+
+        'avg = total / count
+        'Console.WriteLine("Total value= " & total)
+        'Console.Write("Average value= " & avg)
+
+        'Console.ReadKey()
+
+
+    End Sub
+
+End Module
---
+++ train station.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim StationFro, StationTo, NoOfPass, NoOfStats, Fare As Integer
+        Const FarePerPassPerStat = 2
+
+        StationFro = 0
+        StationTo = 0
+        NoOfPass = 0
+        NoOfStats = 0
+        Fare = 0
+
+        Console.Write("From station: ")
+        StationFro = Console.ReadLine
+        Console.Write("To station: ")
+        StationTo = Console.ReadLine
+        Console.Write("Enter number of passengers: ")
+            NoOfPass = Console.ReadLine
+
+        NoOfStats = StationTo - StationFro
+        Fare = NoOfPass * NoOfStats * FarePerPassPerStat
+        If NoOfPass >= 5 And NoOfPass <= 9 Then
+            Console.WriteLine("You are a given a discount of Rs 8")
+            Fare = Fare - 8
+        ElseIf NoOfPass > 9 Then
+            Console.WriteLine("You are given a discount of Rs 15")
+            Fare = Fare - 15
+        End If
+
+        Console.Write("Your fare is: " & Fare)
+        Console.ReadKey()
+
+    End Sub
+
+End Module
